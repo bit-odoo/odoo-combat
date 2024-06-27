f41462a92e0847909f2b1061b02207a4f2871167
parth_kevadiya <kevadiyaparth52004@gmail.com>
2024-06-27 14:28:51 +0530

    first commit

---
+++ backend/odoo/api/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path,include
+from . import views
+
+urlpatterns = [
+    # path('upload/', views.upload_image, name='upload_image'),
+    # path('verify/', views.verify_faces, name='verify_faces'),
+    # path('download_match_csv/', views.download_matches_csv, name='download_matches_csv'),
+]
